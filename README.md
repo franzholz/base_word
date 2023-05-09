b9ee865707653873ca84173d7b966c045eefa390
Franz Holzinger <franz.holzinger@festwein.de>
2023-05-09 09:32:57 +0200

    supports TYPO3 12

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-base_word
-==========
-This provides the PHPWord library from phpOffice. 
+# TYPO3 extension base_word
 
+## What is does
+
+This extension brings the "PHPWord library" classes "phpoffice/phpword" to TYPO3.
+
+## What is the origin
+
+See https://github.com/PHPOffice/PHPWord/tree/1.0.0
+
+## Documentation
+
+https://github.com/PHPOffice/PHPWord/tree/1.0.0
--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'title'            => 'PHP Word Library',
     'description'      => 'This provides the PHPWord library from phpOffice.',
     'category'         => 'misc',
-    'version'          => '0.0.1',
+    'version'          => '1.0.0',
     'state'            => 'stable',
     'clearcacheonload' => 1,
     'author'           => 'PHPWord developers, Franz Holzinger',
@@ -16,12 +16,12 @@ $EM_CONF[$_EXTKEY] = [
     'author_company'   => 'jambage.com',
     'constraints'      => [
         'depends'   => [
-            'php'   => '7.3.0-8.1.99',
-            'typo3' => '10.4.0-11.5.99',
+            'typo3' => '10.4.0-12.4.99',
         ],
         'conflicts' => [
         ],
         'suggests'  => [
+            'escaper' => ''
         ],
     ],
 ];
